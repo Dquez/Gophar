f0d6a8d151c917b69cb09edd0e8acd6f97b49ddc
Dariell Vasquez <dariellv@yahoo.com>
2018-10-07 10:00:07 -0400

    Added parsing functionality for the Clarifai prediction

--- client/src/components/UploadDestination/API.js
+++ client/src/components/UploadDestination/API.js
@@ -11,10 +11,23 @@ export default {
         // return axios.post("/upload", images=> { 
         // predict the contents of an image by passing in a url
         // console.log(image[0].name);
-        const imagesArr = images.split(",");
-        app.models.predict('eee28c313d69466f836ab83287a54ed9', imagesArr).then(
-            function (response) {
-                console.log(response);
+        let responseConcepts = {};
+        const imagesArr = images.replace(" ", "").split(",");
+        app.models.predict('eee28c313d69466f836ab83287a54ed9', imagesArr).then(responses => {
+                console.log(responses.outputs, 1);
+                responses.outputs.forEach(response =>{
+                    const nextTripParams = response.data.concepts.filter(concept=> {
+                        return concept.value > 0.3;
+                    });
+                    nextTripParams.forEach(concept=>{
+                        // if the concept name "view" for example already exists and the value is higher, then dont add the new value
+                        if(!responseConcepts[concept.name] || responseConcepts[concept.name] < concept.value){
+                            responseConcepts[concept.name] = concept.value
+                        }
+                    });
+                });
+                console.log(responseConcepts);
+                
             },
             function (err) {
                 console.error(err);
